5d020f4cfd0028ecd12954f2e2176aa126fe1988
GregDeveaux <deveauxgreg@gmail.com>
2022-12-08 21:43:26 +0100

    modify UIMenu with enum

--- LeBaluchon/Controller/Translate/TranslateViewController+Language.swift
+++ LeBaluchon/Controller/Translate/TranslateViewController+Language.swift
@@ -15,38 +15,132 @@ extension TranslateViewController {
 
     func setupActionsMenu(of languageButton: UIButton, localeLanguage: String, codeLanguage: String) {
 
-            // Create first language of menu
-        let firstLanguage = UIAction(title: localeLanguage) { _ in
-            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: codeLanguage)
+            // Create different pieces of menu
+        let french = UIAction(title: Language.French.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage:  Language.French.code)
         }
 
-            // Take the list of language of the API DeepL
-        let listOfLanguages = Language.list
-
-            // Take anarray empty for the actions menu (list of roll menu)
-        var actions: [UIAction] = []
-
-        for (key, value) in listOfLanguages {
-          print("🎟 \(key): \(value)")
-
-            let language = UIAction(title: value) { _ in
-                self.recognizeButtonLanguageTapped(languageButton, codeLanguage: key)
-            }
-            actions.append(language)
+        let english = UIAction(title: Language.English.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.English.code)
         }
 
-        print("🎟🎟🎟🎟🎟🎟🎟🎟 \(actions)")
+        let italian = UIAction(title: Language.Italian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Italian.code)
+        }
+
+        let spanish = UIAction(title: Language.Spanish.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Spanish.code)
+        }
+
+        let portuguese = UIAction(title: Language.Portuguese.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Portuguese.code)
+        }
+
+        let bulgarian = UIAction(title: Language.Bulgarian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Bulgarian.code)
+        }
+
+        let chinese = UIAction(title: Language.Chinese.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Chinese.code)
+        }
+
+        let czech = UIAction(title: Language.Czech.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Czech.code)
+        }
+
+        let danish = UIAction(title: Language.Danish.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Danish.code)
+        }
+
+        let dutch = UIAction(title: Language.Dutch.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Dutch.code)
+        }
+
+        let german = UIAction(title: Language.German.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.German.code)
+        }
+
+        let greek = UIAction(title: Language.Greek.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Greek.code)
+        }
+
+        let estonian = UIAction(title: Language.Estonian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Estonian.code)
+        }
+
+        let finnish = UIAction(title: Language.Finnish.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Finnish.code)
+        }
+
+        let hungarian = UIAction(title: Language.Hungarian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Hungarian.code)
+        }
+
+        let indonesian = UIAction(title: Language.Indonesian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Indonesian.code)
+        }
+
+        let japanese = UIAction(title: Language.Japanese.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Japanese.code)
+        }
+
+        let lithuanian = UIAction(title: Language.Lithuanian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Lithuanian.code)
+        }
+
+        let latvian = UIAction(title: Language.Latvian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Latvian.code)
+        }
+
+        let polish = UIAction(title: Language.Polish.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Polish.code)
+        }
+
+        let romanian = UIAction(title: Language.Romanian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Romanian.code)
+        }
+
+        let russian = UIAction(title: Language.Russian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Russian.code)
+        }
+
+        let slovak = UIAction(title: Language.Slovak.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Slovak.code)
+        }
+
+        let slovenian = UIAction(title: Language.Slovenian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Slovenian.code)
+        }
+
+        let swedish = UIAction(title: Language.Swedish.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Swedish.code)
+        }
+
+        let turkish = UIAction(title: Language.Turkish.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Turkish.code)
+        }
+
+        let ukrainian = UIAction(title: Language.Ukrainian.rawValue, state: .off) { _ in
+            self.recognizeButtonLanguageTapped(languageButton, codeLanguage: Language.Ukrainian.code)
+        }
+
+        let actions: [UIAction] = [french, english, italian, spanish, portuguese, bulgarian, chinese, czech, danish, dutch,
+                                   german, greek, estonian, finnish, hungarian, indonesian, japanese, lithuanian, latvian,
+                                   polish, romanian, russian, slovak, slovenian, swedish, turkish, ukrainian]
+
+        if let index = actions.firstIndex(where: {languageButton.currentTitle == $0.title}) {
+            actions[index].state = .on
+        }
+
+//        let actionsWithdeleteLanguageChooseInOtherButton = actions.filter({$0.title != currentTitleOtherButton})
 
         let menu = UIMenu(identifier: .text, options: .singleSelection, children: actions)
 
-            // Menu activation 
+            // Menu activation
         languageButton.menu = menu
         languageButton.showsMenuAsPrimaryAction = true
         languageButton.changesSelectionAsPrimaryAction = true
 
-//            // Delete the first language of button
-//        let newListOfLanguages = listOfLanguages.filter({$0.value != localeLanguage})
-
-
+        print("✅ the title of button language -> 🎟 \(languageButton.currentTitle ?? "nothing")")
     }
 }
--- LeBaluchon/Model/ListOfLanguages.swift
+++ LeBaluchon/Model/ListOfLanguages.swift
@@ -2,39 +2,96 @@
 //  ListOfLanguages.swift
 //  LeBaluchon
 //
-//  Created by Greg-Mini on 07/12/2022.
+//  Created by Greg Deveaux on 07/12/2022.
 //
 
 import Foundation
 
-class Language {
-    static var list: [String : String] = [
-            "FR": "French",
-            "EN": "English",
-            "IT": "Italian",
-            "ES": "Spanish",
-            "PT": "Portuguese",
-            "BG": "Bulgarian",
-            "ZH": "Chinese",
-            "CS": "Czech",
-            "DA": "Danish",
-            "NL": "Dutch",
-            "DE": "German",
-            "EL": "Greek",
-            "ET": "Estonian",
-            "FI": "Finnish",
-            "HU": "Hungarian",
-            "ID": "Indonesian",
-            "JA": "Japanese",
-            "LT": "Lithuanian",
-            "LV": "Latvian",
-            "PL": "Polish",
-            "RO": "Romanian",
-            "RU": "Russian",
-            "SK": "Slovak",
-            "SL": "Slovenian",
-            "SV": "Swedish",
-            "TR": "Turkish",
-            "UK": "Ukrainian"
-    ]
+enum Language: String {
+    case French
+    case English
+    case Italian
+    case Spanish
+    case Portuguese
+    case Bulgarian
+    case Chinese
+    case Czech
+    case Danish
+    case Dutch
+    case German
+    case Greek
+    case Estonian
+    case Finnish
+    case Hungarian
+    case Indonesian
+    case Japanese
+    case Lithuanian
+    case Latvian
+    case Polish
+    case Romanian
+    case Russian
+    case Slovak
+    case Slovenian
+    case Swedish
+    case Turkish
+    case Ukrainian
+
+    var code: String {
+        switch self {
+            case .French:
+                return "FR"
+            case .English:
+                return "EN"
+            case .Italian:
+                return "IT"
+            case .Spanish:
+                return "ES"
+            case .Portuguese:
+                return "PT"
+            case .Bulgarian:
+                return "BG"
+            case .Chinese:
+                return "ZH"
+            case .Czech:
+                return "CS"
+            case .Danish:
+                return "DA"
+            case .Dutch:
+                return "NL"
+            case .German:
+                return "DE"
+            case .Greek:
+                return "EL"
+            case .Estonian:
+                return "ET"
+            case .Finnish:
+                return "FI"
+            case .Hungarian:
+                return "HU"
+            case .Indonesian:
+                return "ID"
+            case .Japanese:
+                return "JA"
+            case .Lithuanian:
+                return "LT"
+            case .Latvian:
+                return "LV"
+            case .Polish:
+                return "PL"
+            case .Romanian:
+                return "RO"
+            case .Russian:
+                return "RU"
+            case .Slovak:
+                return "SK"
+            case .Slovenian:
+                return "SL"
+            case .Swedish:
+                return "SV"
+            case .Turkish:
+                return "TR"
+            case .Ukrainian:
+                return "UK"
+        }
+    }
 }
